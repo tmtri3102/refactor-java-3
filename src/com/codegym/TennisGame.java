1efc48c454b94d2c18c64f98f0b69455b3fa6b84
tmtri3102 <tmtri310251@gmail.com>
2024-10-18 12:04:58 +0700

    Refactor

--- src/com/codegym/TennisGame.java
+++ src/com/codegym/TennisGame.java
@@ -1,63 +1,132 @@
+//package com.codegym;
+//
+//public class TennisGame {
+//
+//    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+//        String score = "";
+//        int tempScore=0;
+//        if (m_score1==m_score2)
+//        {
+//            switch (m_score1)
+//            {
+//                case 0:
+//                    score = "Love-All";
+//                    break;
+//                case 1:
+//                    score = "Fifteen-All";
+//                    break;
+//                case 2:
+//                    score = "Thirty-All";
+//                    break;
+//                case 3:
+//                    score = "Forty-All";
+//                    break;
+//                default:
+//                    score = "Deuce";
+//                    break;
+//
+//            }
+//        }
+//        else if (m_score1>=4 || m_score2>=4)
+//        {
+//            int minusResult = m_score1-m_score2;
+//            if (minusResult==1) score ="Advantage player1";
+//            else if (minusResult ==-1) score ="Advantage player2";
+//            else if (minusResult>=2) score = "Win for player1";
+//            else score ="Win for player2";
+//        }
+//        else
+//        {
+//            for (int i=1; i<3; i++)
+//            {
+//                if (i==1) tempScore = m_score1;
+//                else { score+="-"; tempScore = m_score2;}
+//                switch(tempScore)
+//                {
+//                    case 0:
+//                        score+="Love";
+//                        break;
+//                    case 1:
+//                        score+="Fifteen";
+//                        break;
+//                    case 2:
+//                        score+="Thirty";
+//                        break;
+//                    case 3:
+//                        score+="Forty";
+//                        break;
+//                }
+//            }
+//        }
+//        return score;
+//    }
+//}
+
 package com.codegym;
 
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+    private static final int LOVE = 0;
+    private static final int FIFTEEN = 1;
+    private static final int THIRTY = 2;
+    private static final int FORTY = 3;
+    private static final int MINIMUM_WIN_SCORE = 4;
 
-            }
+    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
+        if (player1Score == player2Score) {
+            return getEqualScore(player1Score);
+        } else if (player1Score >= MINIMUM_WIN_SCORE || player2Score >= MINIMUM_WIN_SCORE) {
+            return getAdvantageOrWin(player1Score, player2Score);
+        } else {
+            return getStandardScore(player1Score, player2Score);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+    }
+
+    private static String getEqualScore(int score) {
+        switch (score) {
+            case LOVE:
+                return "Love-All";
+            case FIFTEEN:
+                return "Fifteen-All";
+            case THIRTY:
+                return "Thirty-All";
+            case FORTY:
+                return "Forty-All";
+            default:
+                return "Deuce";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+    }
+
+    private static String getAdvantageOrWin(int player1Score, int player2Score) {
+        int scoreDifference = player1Score - player2Score;
+
+        if (scoreDifference == 1) {
+            return "Advantage player1";
+        } else if (scoreDifference == -1) {
+            return "Advantage player2";
+        } else if (scoreDifference >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
         }
-        return score;
+    }
+
+    private static String getStandardScore(int player1Score, int player2Score) {
+        return getScoreDescription(player1Score) + "-" + getScoreDescription(player2Score);
+    }
+
+    private static String getScoreDescription(int score) {
+        switch (score) {
+            case LOVE:
+                return "Love";
+            case FIFTEEN:
+                return "Fifteen";
+            case THIRTY:
+                return "Thirty";
+            case FORTY:
+                return "Forty";
+            default:
+                throw new IllegalArgumentException("Invalid score: " + score);
+        }
     }
 }
